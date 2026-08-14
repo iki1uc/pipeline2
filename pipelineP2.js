48743a1b42d6590066d4d7497162e76b2b6c4600
iki1uc <entertrainer@web.de>
2026-08-14 18:40:21 +0200

    Update pipelineP2.js

--- pipelineP2.js
+++ pipelineP2.js
@@ -1,1 +1,16 @@
+export const P2 = {
+    run(core){
+        return {
+            id: "P2:P2",
+            phase: "vectorize",
+            vector: {
+                x: Math.random(),
+                y: Math.random(),
+                z: Math.random()
+            },
+            ready: true,
+            core
+        };
+    }
+};
 
